a8d996d75bd978c2975149954e48b474823596aa
codeclan student <codeclanstudent@codeclans-MacBook-Pro-2.local>
2022-11-07 14:38:17 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+intro_text = "Hello World!"
+fave_number = 666
+my_name = "Rita"
+my_age = 32.75
+
+intro_sentence = intro_text + " My name is " + my_name + ", I am " + str(my_age) + " years of age and my favourite number is " + str(fave_number)
+print(intro_sentence)
+
